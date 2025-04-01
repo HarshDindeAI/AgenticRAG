d9720bfb2b63d361eab785a648931cb3c1ee5642
HarshDindeAI <harshdinde3737@gmail.com>
2025-04-01 09:17:40 +0530

    Add Flask backend with routes for serving static files and handling user queries through RAG agent

---
+++ app/backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request,  send_from_directory
+from flask_cors import CORS
+from agent import RAGagent
+import os
+import dotenv
+dotenv.load_dotenv()
+app = Flask(__name__, static_folder="static")
+CORS(app)  # Enable CORS for all routes
+
+@app.route("/")
+def serve_index():
+    return send_from_directory("static", "index.html")
+
+@app.route("/<path:path>")
+def serve_static(path):
+    return send_from_directory("static", path)
+
+@app.route("/init")
+async def init():
+    app.config['agent'] = RAGagent()
+    return jsonify({"status": "success"}), 200
+
+@app.route("/ask", methods=["POST"])
+async def ask():
+    question = request.json["question"]
+    agent: RAGagent = RAGagent()
+    answer = await agent.kickoff(question)
+    print(answer)
+    return {"answer": str(answer)}, 200
+
+if __name__ == "__main__":
+    app.run()
