a7b2a47d4006296ec9808de9200902558b9709a7
HarshDindeAI <harshdinde3737@gmail.com>
2025-04-05 20:02:02 +0530

    Refactor RAG agent to implement a multi-agent workflow with distinct roles for RootAgent, QueryAgent, and AssistentAgent. Update system prompts for improved clarity and functionality in handling user queries and search processes.

--- app/backend/agent.py
+++ app/backend/agent.py
@@ -1,72 +1,83 @@
 import asyncio
-from llama_index.core.agent.workflow import FunctionAgent
+from llama_index.core.agent.workflow import FunctionAgent, AgentWorkflow
 from llama_index.llms.gemini import Gemini
+from llama_index.llms.ollama import Ollama
 import os
 from search import SearchTool
 from llama_index.core.llms import ChatMessage
 
-system_prompt = """
-You are a helpful assistant that retrieves relevant context from a knowledge base by generating customized search queries. Your goal is to provide accurate, relevant, and concise responses based on semantic search results from a vector database.
+root_agent_system_prompt = """
+You are a root agent. You will interact with user and assist him with the answer.
+Your job is to handover the user query to 'QueryAgent' it will break the query to multiple search queries.
+You will get the answer from 'AssistentAgent' till then do not terminate the conversation.
+If you think the answer is satisfied, you can end the conversation.
+"""
 
-Workflow:
-Understanding the User Query:
 
-Analyze the user's question to extract key topics, entities, and intent.
+assistent_system_prompt = """
+You are a helpful assistant.Your job is to search the information in knowledge base using search tool and give the answer to the user. 
+Follow following rules:
+    - You will get saerch strings from 'QueryAgent'.
+    - Use search tool retrive information from knowledge base for all saerch strings.
+    - Answer based on the information retrived from knowledge base.
+After generating asnwer pass the answer to 'RootAgent'.
+"""
 
-Generating Three Search Strings:
 
-Create three different variations of search queries to maximize coverage of relevant information. These queries should be:
+query_agent_system_prompt = """
+You are a Query Agent. You will get the user query from 'Root Agent' and generate search queries for 'AssistentAgent'. Your job is to breakdown the user query into multiple a search querys.
+Follow following rules:
+    - Use a maximum of 3 search queries.
+    - Make sure that these quries will fetch diffrent relivent data from knowledge base.
+    - Breakdown the user query into multiple specific search queries.
+    - Generate search queries in english language. 
+    - Handover this json to 'AssistentAgent'.
+    - Never directly TERMINATE the conversation.
 
-Direct Match Query: Closely aligned with the user’s question.
-
-Reworded Query: A paraphrased version emphasizing different aspects.
-
-Contextual Expansion Query: A broader or related query that captures additional context.
-
-Searching the Vector Database:
-
-Use the three generated search strings to perform a semantic search in the vector database.
-
-Retrieve the top relevant results from each query.
-
-Synthesizing the Answer:
-
-Combine the retrieved information.
-
-Prioritize relevance and coherence.
-
-Provide a concise and accurate response to the user.
-
-Handling Uncertainty:
-
-If search results are unclear or insufficient, inform the user and suggest refining the query."""
+"""
 
 class RAGagent():
     def __init__(self):
         """
         Initializes a RAGagent instance with specified parameters.
 
-        Args:
-            model (str): The model name to be used by the agent. Defaults to "gpt-3.5-turbo".
-            temperature (float): The temperature setting for the model. Defaults to 0.1.
-            api_key (str): The API key for accessing the model. Defaults to an empty string.
-            collection_name (str): The name of the collection for the vector store. Defaults to "my_docs".
-            connection (str): The connection string for the database. Defaults to an empty string.
         """
         self.tool = SearchTool()
         self.llm = Gemini(
             model=os.getenv("MODEL"),
             api_key= os.getenv("API_KEY"),  # uses GOOGLE_API_KEY env var by default
         )
-        self.agent = FunctionAgent(
+        
+        self.root_agent = FunctionAgent(
+            name = "RootAgent",
+            description="Useful for interacting with user",
+            llm=self.llm,
+            system_prompt=root_agent_system_prompt,
+            can_handoff_to=['QueryAgent'],
+        )
+
+        self.query_agent = FunctionAgent(
+            name = "QueryAgent",
+            description="Useful for generating search queries",
+            llm=self.llm,
+            system_prompt=query_agent_system_prompt,
+            can_handoff_to=['AssistentAgent'],
+        )
+
+        self.assistent_agent = FunctionAgent(
+            name = "AssistentAgent",
+            description="Useful for generating answers by retriving knowladge using search tool",
             tools=[self.tool.search],
             llm=self.llm,
-            system_prompt=system_prompt,
+            system_prompt=assistent_system_prompt,
+            can_handoff_to=['RootAgent'],
         )
+
+        self.workflow = AgentWorkflow(agents=[self.root_agent,self.query_agent, self.assistent_agent],root_agent=self.root_agent.name )
 
     async def kickoff(self, question, history):
         history_list = []
         for message in history:
             history_list.append(ChatMessage(content=message["content"], role=message["role"]))
-        response = await self.agent.run(user_msg=question, chat_history=history_list)
+        response = await self.workflow.run(user_msg=question, chat_history=history_list)
         return response
